95180d6a838fbff0ed894a9f0444a6082bfcc168
Antonios Chrysakis <a.chrysakis@gmail.com>
2022-01-10 16:14:36 +0200

    Base

---
+++ screens/CategoryMealsScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Styles from '../components/Styles';
+
+const CategoryMealScreen = props => {
+  return (
+    <View style={Styles.screen}>
+      <Text>The Category Meal Screen</Text>
+    </View>
+  );
+};
+
+export default CategoryMealScreen;
---
+++ screens/FavoritesScreen.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import Styles from '../components/Styles';
+
+const FavoritesScreen = props => {
+  return (
+    <View style={Styles.screen}>
+      <Text>The Favorites Screen</Text>
+    </View>
+  );
+};
+
+export default FavoritesScreen;
